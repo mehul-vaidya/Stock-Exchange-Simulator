ff5b0c6f3164d0f43b96be637357f208acf6cb63
mehul <mehulvaidya94@gmail.com>
2026-02-23 01:27:43 +0530

    bot issue fixed, fixed price and user-order services issue

---
+++ user-order-service/src/main/java/com/UserOrderServiceApplication.java
@@ -0,0 +1,19 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class UserOrderServiceApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PriceServiceApplication.class, args);
+    }
+
+
+}
